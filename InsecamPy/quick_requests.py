44ec1a514ba0db3cbf85587646d8fe8689432462
OEUG99 <iam@oweneugenio.com>
2021-09-17 19:57:54 -0400

    Added Comments.

--- InsecamPy/quick_requests.py
+++ InsecamPy/quick_requests.py
@@ -3,6 +3,13 @@ class QuickRequests():
 
     @classmethod
     async def get(self, url: str, header:dict=None, json=False):
+        """ Function designed for making aiohttp a bit easier, basically sends a get requests.
+
+        :param url:
+        :param header:
+        :param json: Bool. Determines if the output should be in json format or not.
+        :return:
+        """
         connector = aiohttp.TCPConnector()
         async with aiohttp.ClientSession(connector=connector) as session:
             async with session.get(url, headers=header, allow_redirects=True) as resp:
@@ -16,6 +23,13 @@ class QuickRequests():
 
     @classmethod
     async def get_header(self, url: str, header:dict = None):
+        """ Function designed for making aiohttp a bit easier, basically a way to get the content type of a header.
+        Useful for checking if an image is a jpeg.
+
+        :param url:
+        :param header:
+        :return:
+        """
         connector = aiohttp.TCPConnector()
         async with aiohttp.ClientSession(connector=connector) as session:
             try:
